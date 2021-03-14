0c0c44676b024f7252d3864cec86a2fd1e005718
Кирилл <ma-kir.ru@yandex.ru>
2021-03-14 20:24:08 +0300

    secrets added

--- foodgram/settings.py
+++ foodgram/settings.py
@@ -1,14 +1,13 @@
 import os
 from django.urls import reverse_lazy
+from dotenv import load_dotenv
+
+
+load_dotenv()
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'oi9a#msl2kg4bb$v=b2ky@l8-a_m+!tqdgwgi9m!@v_tcixg+z'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -138,7 +137,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'foodgram.django@gmail.com'
-EMAIL_HOST_PASSWORD = 'Djangoproject'
+EMAIL_HOST_PASSWORD = os.getenv('MAIL_PASSWORD')
 EMAIL_USE_TLS = True
 
 
